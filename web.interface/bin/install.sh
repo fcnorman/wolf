3d771c124bd81364b505d1b9059686afbdf671e7
Frank Norman <fcnorman@gmail.com>
2016-06-14 01:57:07 +0000

    Hid flot files

--- web.interface/bin/install.sh
+++ web.interface/bin/install.sh
@@ -5,5 +5,6 @@ sudo apt-get install php5-cli
 sudo wget http://www.flotcharts.org/downloads/flot-0.8.3.zip
 unzip flot-0.8.3.zip
 mv flot ../src/js/flot
+rm -rf flot-0.8.3.zip
 
 
--- web.interface/bin/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd ../src
-
-php -S ec2-52-72-16-7.compute-1.amazonaws.com:8000
-
