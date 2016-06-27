ccaee716253eeb2dbf7827253a87ab41b0671f3c
Frank Norman <fcnorman@gmail.com>
2016-06-27 19:00:35 -0400

    Updates for data.router

--- data.router/bin/run.sh
+++ data.router/bin/run.sh
@@ -1,26 +1,22 @@
 #!/bin/bash
 
-# old
-#./kafka_2.9.2-0.8.1.1/bin/zookeeper-server-start.sh ./kafka_2.9.2-0.8.1.1/config/zookeeper.properties &> zookeeper.log &
-#./kafka_2.9.2-0.8.1.1/bin/kafka-server-start.sh ./kafka_2.9.2-0.8.1.1/config/server.properties &> kafka.log &
-#sleep 20
+ZK_HOST=`cat .zkhost.properties`
+export ZK_HOST=$ZK_HOST
 
-#./kafka_2.9.2-0.8.1.1/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic rules
-#./kafka_2.9.2-0.8.1.1/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic forex
-#./kafka_2.9.2-0.8.1.1/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic forexJ
-
-# new
 ./kafka_2.11-0.10.0.0/bin/zookeeper-server-start.sh \
-    ./kafka_2.9.2-0.8.1.1/config/zookeeper.properties &> zookeeper.log &
-./kafka_2.11-0.10.0.0/bin/kafka-server-start.sh \
-    ./kafka_2.9.2-0.8.1.1/config/server.properties &> kafka.log &
+    ./kafka_2.11-0.10.0.0/config/zookeeper.properties &> zookeeper.log &
 
 sleep 20
 
-./kafka_2.11-0.10.0.0/bin/kafka-topics.sh --create --zookeeper ec2-52-72-16-7.compute-1.amazonaws.com:2181 \
+./kafka_2.11-0.10.0.0/bin/kafka-server-start.sh \
+    ./kafka_2.11-0.10.0.0/config/server.properties &> kafka.log &
+
+sleep 20
+
+./kafka_2.11-0.10.0.0/bin/kafka-topics.sh --create --zookeeper $ZK_HOST:2181 \
     --replication-factor 1 --partitions 1 --topic rules
-./kafka_2.11-0.10.0.0/bin/kafka-topics.sh --create --zookeeper ec2-52-72-16-7.compute-1.amazonaws.com:2181 \
+./kafka_2.11-0.10.0.0/bin/kafka-topics.sh --create --zookeeper $ZK_HOST:2181 \
     --replication-factor 1 --partitions 1 --topic forex
-./kafka_2.11-0.10.0.0/bin/kafka-topics.sh --create --zookeeper ec2-52-72-16-7.compute-1.amazonaws.com:2181 \
+./kafka_2.11-0.10.0.0/bin/kafka-topics.sh --create --zookeeper $ZK_HOST:2181 \
     --replication-factor 1 --partitions 1 --topic forexJ
 
