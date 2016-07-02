765b81c2e86629f3a101c11aa1685b8d3638f7d6
Frank Norman <fcnorman@gmail.com>
2016-07-02 19:16:52 -0400

    Another fix for PublishHistTicks

--- data.provider.hist/src/main/groovy/com/fcnlabs/hist/PublishHistTicks.groovy
+++ data.provider.hist/src/main/groovy/com/fcnlabs/hist/PublishHistTicks.groovy
@@ -79,10 +79,10 @@ class PublishHistTicks {
                     for (record in csv.iterator()) {
 
                         // Create message to be sent to "tick_topic" topic with the tick
-                        KeyedMessage<String, String> data = new KeyedMessage<String, String>("histticks", record)
+                        KeyedMessage<String, String> data = new KeyedMessage<String, String>("histticks", record.values.toString())
 
                         // Send the message
-                        producer.send(record)
+                        producer.send(record.values.toString())
 
                         processedRecords++
 
