3877549658fd2d3e28f453fcb20760f68f873cbc
Frank Norman <fcnorman@gmail.com>
2016-07-02 19:48:28 -0400

    Faster data.provider.hist

--- data.provider.hist/src/main/groovy/com/fcnlabs/hist/PublishHistTicks.groovy
+++ data.provider.hist/src/main/groovy/com/fcnlabs/hist/PublishHistTicks.groovy
@@ -6,25 +6,15 @@ import kafka.producer.ProducerConfig
 import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.Logger
 
-import java.nio.file.Paths
-import java.time.LocalDateTime
-import java.time.ZonedDateTime
-import java.time.format.DateTimeFormatter
-
 class PublishHistTicks {
 
     private static final Logger log = LogManager.getLogger(PublishHistTicks.class)
 
     public static void main(String[] args) {
-        def env = System.getenv()
-        String cassandra_host = env['CQLSH_HOST']
-        String kafka_host = env['KAFKA_HOST']
-        String histdata_dir = env['HISTDATA_DIR']
-        ZonedDateTime zonedDateTime = null
-        LocalDateTime localDateTime = null
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd' 'HHmmssSSS")
-        DateTimeFormatter cassie_formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'000Z'")
-        String fcnDateTime = null
+        def env                 = System.getenv()
+        String cassandra_host   = env['CQLSH_HOST']
+        String kafka_host       = env['KAFKA_HOST']
+        String histdata_dir     = env['HISTDATA_DIR']
 
         final String ipAddress = kafka_host
         final int port = args.length > 1 ? Integer.parseInt(args[1]) : 9092
@@ -59,42 +49,41 @@ class PublishHistTicks {
                 if (j < 10) { jj = "0" + jj }
 
                 Integer totalRecords = 0
-                log.info("Processing " + histdata_dir + '/DAT_ASCII_EURUSD_T_' + i.toString() + jj + '.csv')
 
-                Paths.get(histdata_dir + '/DAT_ASCII_EURUSD_T_' + i.toString() + jj + '.csv').withReader { reader ->
-                    org.apache.commons.csv.CSVParser csv = new org.apache.commons.csv.CSVParser(reader, org.apache.commons.csv.CSVFormat.DEFAULT)
+                String filename = histdata_dir + '/DAT_ASCII_EURUSD_T_' + i.toString() + jj + '.csv'
 
-                    for (record in csv.iterator()) {
-                        totalRecords++
-                    }
+                log.info("Processing " + filename)
+
+                File file = new File ( filename )
+
+                file.eachLine { line ->
+                    totalRecords++
                 }
+
+                File fileSecondPass = new File ( filename )
 
                 Integer processedRecords = 0
                 Double tickler = 0.10
                 Date startTime = new Date()
-                Paths.get(histdata_dir + '/DAT_ASCII_EURUSD_T_' + i.toString() + jj + '.csv').withReader { reader ->
-                    org.apache.commons.csv.CSVParser csv = new org.apache.commons.csv.CSVParser(reader, org.apache.commons.csv.CSVFormat.DEFAULT)
+                fileSecondPass.eachLine { line ->
 
                     log.info("Start time: " + new Date().toString())
-                    for (record in csv.iterator()) {
 
-                        // Create message to be sent to "tick_topic" topic with the tick
-                        KeyedMessage<String, String> data = new KeyedMessage<String, String>("histticks", record.values[0].toString())
+                    // Create message to be sent to "tick_topic" topic with the tick
+                    KeyedMessage<String, String> data = new KeyedMessage<String, String>("histticks", line)
 
-                        // Send the message
-                        producer.send(record.values[0].toString())
+                    // Send the message
+                    producer.send(line)
 
-                        processedRecords++
+                    processedRecords++
 
-                        if ((processedRecords / totalRecords) > tickler) {
-                            log.info((tickler * 100).toString() + "% records processed.  " + processedRecords.toString() + " / " + totalRecords.toString())
-                            tickler = tickler + 0.10
-                            Date intervalTime = new Date()
-                            // interval between two times in milliseconds
-                            Long interimInterval = intervalTime.getTime() - startTime.getTime()
-                            log.info("Interval time: " + intervalTime.toString() + " Transactions Per Minute: " + (processedRecords / (interimInterval * 60000)).toString())
-
-                        }
+                    if ((processedRecords / totalRecords) > tickler) {
+                        log.info((tickler * 100).toString() + "% records processed.  " + processedRecords.toString() + " / " + totalRecords.toString())
+                        tickler = tickler + 0.10
+                        Date intervalTime = new Date()
+                        // interval between two times in milliseconds
+                        Long interimInterval = intervalTime.getTime() - startTime.getTime()
+                        log.info("Interval time: " + intervalTime.toString() + " Transactions Per Minute: " + (processedRecords / (interimInterval * 60000)).toString())
                     }
                 }
                 Date endTime = new Date()
